6bd74b403a1e733465027dcf9f129712714e01bd
Jakub Wojcik <wcuba@wp.pl>
2025-03-30 13:20:27 +0200

    finished navbar and tollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ToolbarComponent } from './components/shared/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [MatToolbarModule,MatButtonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
